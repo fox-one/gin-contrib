2ddebda834f702af8c0711a9a4c6f62301da17ef
yiplee <guoyinl@gmail.com>
2019-02-25 19:17:53 +0800

    Mod: _

--- gin_helper/logger.go
+++ gin_helper/logger.go
@@ -9,29 +9,31 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func Logger(c *gin.Context) {
-	start := time.Now()
-	c.Next()
+func Log(in *time.Location) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now().In(in)
+		c.Next()
 
-	end := time.Now()
-	status := c.Writer.Status()
-	method := c.Request.Method
-	uri := c.Request.URL.String()
+		end := time.Now()
+		status := c.Writer.Status()
+		method := c.Request.Method
+		uri := c.Request.URL.String()
 
-	content := fmt.Sprintf("[%d] %-4s %s", status, method, uri)
+		content := fmt.Sprintf("[%d] %-4s %s", status, method, uri)
 
-	entry := log.WithFields(log.Fields{
-		"ts": end.Format(time.RFC3339),
-		"lt": end.Sub(start),
-		"ip": c.ClientIP(),
-		"ua": c.Request.UserAgent(),
-	})
+		entry := log.WithFields(log.Fields{
+			"ts": start.Format(time.RFC3339),
+			"lt": end.Sub(start),
+			"ip": c.ClientIP(),
+			"ua": c.Request.UserAgent(),
+		})
 
-	if status >= http.StatusOK && status < 300 {
-		entry.Info(content)
-	} else if status == http.StatusNotFound {
-		entry.Warn(content)
-	} else {
-		entry.Error(content)
+		if status >= http.StatusOK && status < 300 {
+			entry.Info(content)
+		} else if status == http.StatusNotFound {
+			entry.Warn(content)
+		} else {
+			entry.Error(content)
+		}
 	}
 }
