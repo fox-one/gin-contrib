00e787550c4e9764ba1a1d5ad14db03e39a30d29
yiplee <guoyinl@gmail.com>
2019-03-11 21:08:39 +0800

    Mod: _

--- limiter/blocker.go
+++ limiter/blocker.go
@@ -2,6 +2,7 @@ package limiter
 
 import (
 	"strconv"
+	"strings"
 	"time"
 
 	uuid "github.com/satori/go.uuid"
@@ -10,8 +11,8 @@ import (
 )
 
 type Blocker interface {
-	BlockUntil(id string, exp time.Time) error
-	State(id string) (exp time.Time, blocked bool)
+	BlockUntil(id, cause string, exp time.Time) error
+	State(id string) (exp time.Time, cause string, blocked bool)
 	Clean(id string) error
 }
 
@@ -28,7 +29,7 @@ func (b *sortedSetBlocker) key(id string) string {
 	return "limiter:blocker:" + id
 }
 
-func (b *sortedSetBlocker) BlockUntil(id string, exp time.Time) error {
+func (b *sortedSetBlocker) BlockUntil(id, cause string, exp time.Time) error {
 	score := exp.Unix()
 	if score <= time.Now().Unix() {
 		return nil
@@ -38,7 +39,7 @@ func (b *sortedSetBlocker) BlockUntil(id string, exp time.Time) error {
 	_, err := b.client.Pipelined(func(p redis.Pipeliner) error {
 		p.ZRemRangeByScore(key, "-inf", strconv.FormatInt(score, 10))
 		p.ZAdd(key, redis.Z{
-			Member: uuid.Must(uuid.NewV4()),
+			Member: uuid.Must(uuid.NewV4()).String() + ":" + cause,
 			Score:  float64(score),
 		})
 		p.Expire(key, b.maxAge)
@@ -47,12 +48,17 @@ func (b *sortedSetBlocker) BlockUntil(id string, exp time.Time) error {
 	return err
 }
 
-func (b *sortedSetBlocker) State(id string) (exp time.Time, blocked bool) {
+func (b *sortedSetBlocker) State(id string) (exp time.Time, cause string, blocked bool) {
 	key := b.key(id)
 	if val := b.client.ZRangeWithScores(key, -1, -1).Val(); len(val) >= 1 {
 		z := val[0]
 		exp = time.Unix(int64(z.Score), 0)
 		blocked = exp.After(time.Now())
+		if member, ok := z.Member.(string); ok {
+			if segments := strings.SplitN(member, ":", 2); len(segments) == 2 {
+				cause = segments[1]
+			}
+		}
 	}
 
 	return
